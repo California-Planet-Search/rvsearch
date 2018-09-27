e61e3bba397c37495db70532a09131c933734872
Lee J. Rosenthal <leerosenthalj@gmail.com>
2018-09-27 14:45:43 -0700

    Added all currently desired functions to the Search class.

--- rvsearch/search.py
+++ rvsearch/search.py
@@ -1,1 +1,27 @@
-#Search class.
+    #Search class.
+
+class Search(object):
+
+    def __init__(self, post):
+        pass
+
+    def add_planet(self, post, default_pdict, data):
+        #Attempting to modify Lea's add_planet function to fit this class.
+        pass
+
+    def sub_planet(self, post):
+        pass
+
+    def save(self, post, filename=None):
+        if filename != None:
+            post.writeto(filename)
+        else:
+            post.writeto('post.pkl')
+        #Write this so that it can be iteratively applied with each planet addition.
+
+    def plot_model(self, post):
+        pass
+
+    def all_posts(self):
+        #Return list of posteriors for each nth planet model
+        pass
