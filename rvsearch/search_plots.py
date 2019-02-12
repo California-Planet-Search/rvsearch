fddb6441c04c430005dd411d5589f943e5fad3a9
Lee J. Rosenthal <leerosenthalj@gmail.com>
2019-02-12 12:00:59 -0800

    Added in a bunch of Erik/BJ MultiPanelPlot code.

--- rvsearch/search_plots.py
+++ rvsearch/search_plots.py
@@ -122,30 +122,202 @@ class SearchPlot(object):
 
     def plot_timeseries(self):
         """Make a plot of the RV data and model in the current Axes.
-        """
 
-        ax = plt.gca()
-        ax.hline(0, color='0.5', linestyle='--')
+        """
+        ax = pl.gca()
+
+        ax.axhline(0, color='0.5', linestyle='--')
+
+        # plot orbit model
+        ax.plot(self.mplttimes,self.orbit_model,'b-', rasterized=False, lw=self.fit_linewidth)
+
+        # plot data
+        plot.mtelplot(
+            # data = residuals + model
+            self.plttimes, self.rawresid+self.rvmod, self.rverr, self.post.likelihood.telvec, ax, telfmts=self.telfmts
+        )
+
+        if self.set_xlim is not None:
+            ax.set_xlim(self.set_xlim)
+        else:
+            ax.set_xlim(min(self.plttimes)-0.01*self.dt, max(self.plttimes)+0.01*self.dt)
+        pl.setp(ax.get_xticklabels(), visible=False)
+
+        # legend
+        if self.legend:
+            ax.legend(numpoints=1, **self.legend_kwargs)
+
+        # years on upper axis
+        axyrs = ax.twiny()
+        xl = np.array(list(ax.get_xlim())) + self.epoch
+        decimalyear = Time(xl, format='jd', scale='utc').decimalyear
+#        axyrs.plot(decimalyear, decimalyear)
+        axyrs.get_xaxis().get_major_formatter().set_useOffset(False)
+        axyrs.set_xlim(*decimalyear)
+        axyrs.set_xlabel('Year', fontweight='bold')
+        pl.locator_params(axis='x', nbins=5)
+
+        if not self.yscale_auto:
+            scale = np.std(self.rawresid+self.rvmod)
+            ax.set_ylim(-self.yscale_sigma * scale, self.yscale_sigma * scale)
+
+
+        ax.set_ylabel('RV [{ms:}]'.format(**plot.latex), weight='bold')
+        ticks = ax.yaxis.get_majorticklocs()
+        ax.yaxis.set_ticks(ticks[1:])
+
+    def plot_residuals(self):
+        """Make a plot of residuals and RV trend in the current Axes.
+
+        """
+        ax = pl.gca()
+
+        ax.plot(self.mplttimes, self.slope, 'b-', lw=self.fit_linewidth)
+
+        plot.mtelplot(self.plttimes, self.resid, self.rverr, self.post.likelihood.telvec, ax, telfmts=self.telfmts)
+        if not self.yscale_auto:
+            scale = np.std(self.resid)
+            ax.set_ylim(-self.yscale_sigma * scale, self.yscale_sigma * scale)
+
+        if self.set_xlim is not None:
+            ax.set_xlim(self.set_xlim)
+        else:
+            ax.set_xlim(min(self.plttimes)-0.01*self.dt, max(self.plttimes)+0.01*self.dt)
+        ticks = ax.yaxis.get_majorticklocs()
+        ax.yaxis.set_ticks([ticks[0], 0.0, ticks[-1]])
+        pl.xlabel('JD - {:d}'.format(int(np.round(self.epoch))), weight='bold')
+        ax.set_ylabel('Residuals', weight='bold')
+        ax.yaxis.set_major_locator(MaxNLocator(5, prune='both'))
 
     def plot_phasefold(self, pltletter, pnum=0):
-        pass
+        """
+        Plot phased orbit plots for each planet in the fit.
 
-    def plot_periodogram(self, pnum=0):
-        peak = np.argmax(self.periodograms[pnum])
-        f_real = 1/self.pers[peak]
+        Args:
+            pltletter (int): integer representation of
+                letter to be printed in the corner of the first
+                phase plot.
+                Ex: ord("a") gives 97, so the input should be 97.
+            pnum (int): the number of the planet to be plotted. Must be
+                the same as the number used to define a planet's
+                Parameter objects (e.g. 'per1' is for planet #1)
+
+        """
+        ax = pl.gca()
+
+        if len(self.post.likelihood.x) < 20:
+            self.nobin = True
+
+        bin_fac = 1.75
+        bin_markersize = bin_fac * rcParams['lines.markersize']
+        bin_markeredgewidth = bin_fac * rcParams['lines.markeredgewidth']
+
+        rvmod2 = self.model(self.rvmodt, planet_num=pnum) - self.slope
+        modph = t_to_phase(self.post.params, self.rvmodt, pnum, cat=True) - 1
+        rvdat = self.rawresid + self.model(self.rvtimes, planet_num=pnum) - self.slope_low
+        phase = t_to_phase(self.post.params, self.rvtimes, pnum, cat=True) - 1
+        rvdatcat = np.concatenate((rvdat, rvdat))
+        rverrcat = np.concatenate((self.rverr, self.rverr))
+        rvmod2cat = np.concatenate((rvmod2, rvmod2))
+        bint, bindat, binerr = fastbin(phase+1, rvdatcat, nbins=25)
+        bint -= 1.0
+
+        ax.axhline(0, color='0.5', linestyle='--', )
+        ax.plot(sorted(modph), rvmod2cat[np.argsort(modph)], 'b-', linewidth=self.fit_linewidth)
+        plot.labelfig(pltletter)
+
+        telcat = np.concatenate((self.post.likelihood.telvec, self.post.likelihood.telvec))
+
+        plot.mtelplot(phase, rvdatcat, rverrcat, telcat, ax, telfmts=self.telfmts)
+        if not self.nobin and len(rvdat) > 10:
+            ax.errorbar(
+                bint, bindat, yerr=binerr, fmt='ro', mec='w', ms=bin_markersize,
+                mew=bin_markeredgewidth
+            )
+
+        if self.phase_limits:
+            ax.set_xlim(self.phase_limits[0],self.phase_limits[1])
+        else:
+            ax.set_xlim(-0.5, 0.5)
+
+        if not self.yscale_auto:
+            scale = np.std(rvdatcat)
+            ax.set_ylim(-self.yscale_sigma*scale, self.yscale_sigma*scale)
+
+        keys = [p+str(pnum) for p in ['per', 'k', 'e']]
+
+        labels = [self.post.params.tex_labels().get(k, k) for k in keys]
+        if pnum < self.num_planets:
+            ticks = ax.yaxis.get_majorticklocs()
+            ax.yaxis.set_ticks(ticks[1:-1])
+
+        ax.set_ylabel('RV [{ms:}]'.format(**plot.latex), weight='bold')
+        ax.set_xlabel('Phase', weight='bold')
+
+        print_params = ['per', 'k', 'e']
+        units = {'per': 'days', 'k': plot.latex['ms'], 'e': ''}
+
+        anotext = []
+        for l, p in enumerate(print_params):
+            val = self.post.params["%s%d" % (print_params[l], pnum)].value
+
+            if self.uparams is None:
+                _anotext = '$\\mathregular{%s}$ = %4.2f %s' % (labels[l].replace("$", ""), val, units[p])
+            else:
+                if hasattr(self.post, 'medparams'):
+                    val = self.post.medparams["%s%d" % (print_params[l], pnum)]
+                else:
+                    print("WARNING: medparams attribute not found in " +
+                          "posterior object will annotate with " +
+                          "max-likelihood values and reported uncertainties " +
+                          "may not be appropriate.")
+                err = self.uparams["%s%d" % (print_params[l], pnum)]
+                if err > 1e-15:
+                    val, err, errlow = sigfig(val, err)
+                    _anotext = '$\\mathregular{%s}$ = %s $\\mathregular{\\pm}$ %s %s' \
+                               % (labels[l].replace("$", ""), val, err, units[p])
+                else:
+                    _anotext = '$\\mathregular{%s}$ = %4.2f %s' % (labels[l].replace("$", ""), val, units[p])
+
+            anotext += [_anotext]
+
+        anotext = '\n'.join(anotext)
+        plot.add_anchored(
+            anotext, loc=1, frameon=True, prop=dict(size=self.phasetext_size, weight='bold'),
+            bbox=dict(ec='none', fc='w', alpha=0.8)
+        )
+
+    def plot_periodogram(self, alias=True, floor=False, save=False):
+        """Plot periodogram.
+
+        Args:
+            alias (bool): Plot year, month, day aliases?
+            floor (bool): Set y-axis minimum according to likelihood limit?
+            save (bool): Save plot to current directory?
+
+        """
+        # TO-DO: WORK IN AIC/BIC OPTION, INCLUDE IN PLOT TITLE
+        peak = np.argmax(self.power['bic'])
+        f_real = self.freqs[peak]
 
         fig, ax = plt.subplots()
-        ax.plot(self.pers, self.periodograms[pnum])
-        ax.scatter(self.pers[peak], self.periodograms[pnum][peak],
-                   label='{} days'.format(np.round(self.pers[peak], decimals=1)))
+        ax.plot(self.pers, self.power['bic'])
+        ax.scatter(self.pers[peak], self.power['bic'][peak], label='{} days'\
+                            .format(np.round(self.pers[peak], decimals=1)))
 
-        # If D-BIC threshold has been calculated, plot.
-        if self.bic_thresh[pnum] is not None:
-            ax.axhline(self.bic_thresh[pnum ], ls=':', c='y', label=r'$\Delta$BIC threshold')
-            upper = 1.05*max(np.amax(self.periodograms[pnum]), self.bic_thresh)
-            ax.set_ylim([np.amin(self.periodograms[pnum]), upper])
+        # If DBIC threshold has been calculated, plot.
+        if self.bic_thresh is not None:
+            ax.axhline(self.bic_thresh, ls=':', c='y', label='{} FAP'\
+                                                    .format(self.fap))
+            upper = 1.1*max(np.amax(self.power['bic']), self.bic_thresh)
         else:
-            ax.set_ylim([np.amin(self.periodograms[pnum]), 1.05*np.amax(self.periodograms[pnum])])
+            upper = 1.1*np.amax(self.power['bic'])
+        if floor:
+            # Set periodogram plot floor according to circular-fit BIC min.
+            lower = -2*np.log(len(self.times))
+        else:
+            lower = np.amin(self.power['bic'])
+        ax.set_ylim([lower, upper])
         ax.set_xlim([self.pers[0], self.pers[-1]])
 
         if alias:
@@ -156,20 +328,16 @@ class SearchPlot(object):
                 f_ap = 1./alias[i] + f_real
                 f_am = 1./alias[i] - f_real
                 ax.axvline(1./f_am, linestyle='--', c=colors[i], alpha=0.5,
-                           label='{} day alias'.format(np.round(alias[i], decimals=1)))
+                                label='{} day alias'.format(np.round(alias[i],
+                                decimals=1)))
                 ax.axvline(1./f_ap, linestyle='--', c=colors[i], alpha=0.5)
 
         ax.legend(loc=0)
         ax.set_xscale('log')
         ax.set_xlabel('Period (days)')
         ax.set_ylabel(r'$\Delta$BIC')  # TO-DO: WORK IN AIC/BIC OPTION
-        ax.set_title('Planet {} vs. planet {}'.format(self.num_known_planets+1, self.num_known_planets))
-
-        # Store figure as object attribute, make separate saving functionality?
-        self.fig = fig
-        if save:
-            # FINISH THIS, WRITE NAMING PROCEDURE
-            fig.savefig('dbic{}.pdf'.format(self.num_known_planets+1))
+        ax.set_title('Planet {} vs. planet {}'.format(self.num_known_planets+1,
+                                                        self.num_known_planets))
 
     def plot_periodograms_orbits(self):
         """Call everything above to construct a multipanel plot.
