5aaec6367a97f21c3bcc7a5fa1e0750c3dd82b35
BJ Fulton <bjfultn@gmail.com>
2019-03-09 14:52:39 -0800

    run_injections pass through args

--- scripts/run_injections.py
+++ scripts/run_injections.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 import os
+import sys
 from glob import glob
 
 from radvel.utils import working_directory
 
 search_list = sorted(glob('./*/search.pkl'))
 
+args = ' '.join(sys.argv[1:])
+
 for sfile in search_list:
     sdir = os.path.abspath(os.path.dirname(sfile))
     sfile = os.path.abspath(sfile)
-    print(sfile)
 
     with working_directory(sdir):
         if not os.path.exists('recoveries.csv'):
-            print("rvsearch inject {}".format(sdir))
+            print("rvsearch inject {} {}".format(args, sdir))
