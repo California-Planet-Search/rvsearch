027382c90d693b8e6e3101100dc262fc7106965b
Lee J. Rosenthal <leerosenthalj@gmail.com>
2019-04-19 16:30:26 -0700

    Brought in summary plot inheritance from BJ.

--- rvsearch/plots.py
+++ rvsearch/plots.py
@@ -3,7 +3,7 @@ import numpy as np
 import pylab as pl
 from matplotlib import pyplot as pl
 from matplotlib import rcParams, gridspec
-from matplotlib.ticker import MaxNLocator
+from matplotlib.ticker import MaxNLocator, LogFormatterSciNotation, FuncFormatter
 from astropy.time import Time
 
 import radvel
@@ -13,7 +13,16 @@ from radvel.utils import t_to_phase, fastbin, sigfig
 import rvsearch.utils as utils
 # IMPORTANT: AT SOME POINT, REDEFINE AS CLASS INHERITING FROM RADVEL MULTIPLOT.
 
-class PeriodModelPlot(object):
+
+ class CustomTicker(LogFormatterSciNotation):
+    def __call__(self, x, pos=None):
+        if x not in [0.1, 1, 10, 100, 1000, 10000]:
+            return LogFormatterSciNotation.__call__(self, x, pos=None)
+        else:
+            return "{x:g}".format(x=x)
+
+
+ class PeriodModelPlot(radvel.plot.orbit_plots.MultipanelPlot):
     """Class to jointly plot the periodograms, best model phaseplots, and
         window function for each search iteration.
 
@@ -144,171 +153,6 @@ class PeriodModelPlot(object):
         # list for Axes objects
         self.ax_list = []
 
-    def plot_timeseries(self):
-        """Make a plot of the RV data and model in the current Axes.
-
-        """
-        ax = pl.gca()
-
-        ax.axhline(0, color='0.5', linestyle='--')
-
-        # plot orbit model
-        ax.plot(self.mplttimes, self.orbit_model, 'b-', rasterized=False, lw=self.fit_linewidth)
-
-        # plot data
-        plot.mtelplot(
-            # data = residuals + model
-            self.plttimes, self.rawresid+self.rvmod, self.rverr, self.post.likelihood.telvec, ax, telfmts=self.telfmts
-        )
-
-        if self.set_xlim is not None:
-            ax.set_xlim(self.set_xlim)
-        else:
-            ax.set_xlim(min(self.plttimes)-0.01*self.dt, max(self.plttimes)+0.01*self.dt)
-        pl.setp(ax.get_xticklabels(), visible=False)
-
-        # legend
-        if self.legend:
-            ax.legend(numpoints=1, **self.legend_kwargs)
-
-        # years on upper axis
-        axyrs = ax.twiny()
-        xl = np.array(list(ax.get_xlim())) + self.epoch
-        decimalyear = Time(xl, format='jd', scale='utc').decimalyear
-        axyrs.get_xaxis().get_major_formatter().set_useOffset(False)
-        axyrs.set_xlim(*decimalyear)
-        axyrs.set_xlabel('{}\nYear'.format(self.starname), fontweight='bold')
-        pl.locator_params(axis='x', nbins=5)
-
-        if not self.yscale_auto:
-            scale = np.std(self.rawresid+self.rvmod)
-            ax.set_ylim(-self.yscale_sigma * scale, self.yscale_sigma * scale)
-
-        ax.set_ylabel('RV [{ms:}]'.format(**plot.latex), weight='bold')
-        ticks = ax.yaxis.get_majorticklocs()
-        ax.yaxis.set_ticks(ticks[1:])
-
-    def plot_residuals(self):
-        """Make a plot of residuals and RV trend in the current Axes.
-
-        """
-        ax = pl.gca()
-
-        ax.plot(self.mplttimes, self.slope, 'b-', lw=self.fit_linewidth)
-
-        plot.mtelplot(self.plttimes, self.resid, self.rverr, self.post.likelihood.telvec, ax, telfmts=self.telfmts)
-        if not self.yscale_auto:
-            scale = np.std(self.resid)
-            ax.set_ylim(-self.yscale_sigma * scale, self.yscale_sigma * scale)
-
-        if self.set_xlim is not None:
-            ax.set_xlim(self.set_xlim)
-        else:
-            ax.set_xlim(min(self.plttimes)-0.01*self.dt, max(self.plttimes)+0.01*self.dt)
-        ticks = ax.yaxis.get_majorticklocs()
-        ax.yaxis.set_ticks([ticks[0], 0.0, ticks[-1]])
-        pl.xlabel('JD - {:d}'.format(int(np.round(self.epoch))), weight='bold')
-        ax.set_ylabel('Residuals', weight='bold')
-        ax.yaxis.set_major_locator(MaxNLocator(5, prune='both'))
-
-    def plot_phasefold(self, pltletter, pnum):
-        """Plot phased orbit plots for each planet in the fit.
-
-        Args:
-            pltletter (int): integer representation of
-                letter to be printed in the corner of the first
-                phase plot.
-                Ex: ord("a") gives 97, so the input should be 97.
-            pnum (int): the number of the planet to be plotted. Must be
-                the same as the number used to define a planet's
-                Parameter objects (e.g. 'per1' is for planet #1)
-
-        """
-        ax = pl.gca()
-
-        if len(self.post.likelihood.x) < 20:
-            self.nobin = True
-
-        bin_fac = 1.75
-        bin_markersize = bin_fac * rcParams['lines.markersize']
-        bin_markeredgewidth = bin_fac * rcParams['lines.markeredgewidth']
-
-        rvmod2 = self.model(self.rvmodt, planet_num=pnum) - self.slope
-        modph = t_to_phase(self.post.params, self.rvmodt, pnum, cat=True) - 1
-        rvdat = self.rawresid + self.model(self.rvtimes, planet_num=pnum) - self.slope_low
-        phase = t_to_phase(self.post.params, self.rvtimes, pnum, cat=True) - 1
-        rvdatcat = np.concatenate((rvdat, rvdat))
-        rverrcat = np.concatenate((self.rverr, self.rverr))
-        rvmod2cat = np.concatenate((rvmod2, rvmod2))
-        bint, bindat, binerr = fastbin(phase+1, rvdatcat, nbins=25)
-        bint -= 1.0
-
-        ax.axhline(0, color='0.5', linestyle='--', )
-        ax.plot(sorted(modph), rvmod2cat[np.argsort(modph)], 'b-', linewidth=self.fit_linewidth)
-        plot.labelfig(pltletter)
-
-        telcat = np.concatenate((self.post.likelihood.telvec, self.post.likelihood.telvec))
-
-        plot.mtelplot(phase, rvdatcat, rverrcat, telcat, ax, telfmts=self.telfmts)
-        if not self.nobin and len(rvdat) > 10:
-            ax.errorbar(
-                bint, bindat, yerr=binerr, fmt='ro', mec='w', ms=bin_markersize,
-                mew=bin_markeredgewidth
-            )
-
-        if self.phase_limits:
-            ax.set_xlim(self.phase_limits[0], self.phase_limits[1])
-        else:
-            ax.set_xlim(-0.5, 0.5)
-
-        if not self.yscale_auto:
-            scale = np.std(rvdatcat)
-            ax.set_ylim(-self.yscale_sigma*scale, self.yscale_sigma*scale)
-
-        keys = [p+str(pnum) for p in ['per', 'k', 'e']]
-
-        labels = [self.post.params.tex_labels().get(k, k) for k in keys]
-        if pnum < self.num_planets:
-            ticks = ax.yaxis.get_majorticklocs()
-            ax.yaxis.set_ticks(ticks[1:-1])
-
-        ax.set_ylabel('RV [{ms:}]'.format(**plot.latex), weight='bold')
-        ax.set_xlabel('Phase', weight='bold')
-
-        print_params = ['per', 'k', 'e']
-        units = {'per': 'days', 'k': plot.latex['ms'], 'e': ''}
-
-        anotext = []
-        for l, p in enumerate(print_params):
-            val = self.post.params["%s%d" % (print_params[l], pnum)].value
-
-            if self.uparams is None:
-                _anotext = '$\\mathregular{%s}$ = %4.2f %s' % (labels[l].replace("$", ""), val, units[p])
-            else:
-                if hasattr(self.post, 'medparams'):
-                    val = self.post.medparams["%s%d" % (print_params[l], pnum)]
-                else:
-                    print("WARNING: medparams attribute not found in " +
-                          "posterior object will annotate with " +
-                          "max-likelihood values and reported uncertainties " +
-                          "may not be appropriate.")
-                err = self.uparams["%s%d" % (print_params[l], pnum)]
-                if err > 1e-15:
-                    val, err, errlow = sigfig(val, err)
-                    _anotext = '$\\mathregular{%s}$ = %s $\\mathregular{\\pm}$ %s %s' \
-                               % (labels[l].replace("$", ""), val, err, units[p])
-                else:
-                    _anotext = '$\\mathregular{%s}$ = %4.2f %s' % (labels[l].replace("$", ""), val, units[p])
-
-            anotext += [_anotext]
-
-        anotext = '\n'.join(anotext)
-        plot.add_anchored(
-            anotext, loc=1, frameon=True, prop=dict(size=self.phasetext_size,
-                                                    weight='bold'),
-            bbox=dict(ec='none', fc='w', alpha=0.8)
-        )
-
     def plot_periodogram(self, pltletter, pnum=0, alias=True, floor=True):
         """Plot periodogram for a given search iteration.
 
@@ -398,6 +242,7 @@ class PeriodModelPlot(object):
             ax.set_xlabel('Period [day]', fontweight='bold')
             ax.tick_params(axis='x', which='both', direction='out',
                            bottom='on', top='off', labelbottom='on')
+	        ax.xaxis.set_major_formatter(CustomTicker())
 
     def plot_window(self, pltletter):
         """Plot the window function of the data.
@@ -427,92 +272,10 @@ class PeriodModelPlot(object):
         ax.set_ylim([0, 1.1*np.amax(window_safe)])
         ax.set_xlim([np.amin(self.pers), np.amax(self.pers)])
         ax.plot(pers_safe, window_safe, c='g')
+        ax.xaxis.set_major_formatter(CustomTicker())
 
         ax.axvspan(np.amin(self.pers), min, alpha=0.25, color='purple')
         ax.axvspan(max, np.amax(self.pers), alpha=0.25, color='purple')
-
-    def plot_multipanel(self, nophase=False, letter_labels=True):
-        """Provision and plot an RV multipanel plot
-
-        Args:
-            nophase (bool, optional): if True, don't
-                include phase plots. Default: False.
-            letter_labels (bool, optional): if True, include
-                letter labels on orbit and residual plots.
-                Default: True.
-
-        Returns:
-            tuple containing:
-                - current matplotlib Figure object
-                - list of Axes objects
-
-        """
-        if nophase:
-            scalefactor = 1
-        else:
-            scalefactor = self.phase_nrows
-
-        figheight = self.ax_rv_height + self.ax_phase_height * scalefactor
-
-        # provision figure
-        fig = pl.figure(figsize=(self.figwidth, figheight))
-
-        fig.subplots_adjust(left=0.12, right=0.95)
-        gs_rv = gridspec.GridSpec(2, 1, height_ratios=[1., 0.5])
-
-        divide = 1 - self.ax_rv_height / figheight
-        gs_rv.update(left=0.12, right=0.93, top=0.93,
-                     bottom=divide+self.rv_phase_space*0.5, hspace=0.)
-
-        # orbit plot
-        ax_rv = pl.subplot(gs_rv[0, 0])
-        self.ax_list += [ax_rv]
-
-        pl.sca(ax_rv)
-        self.plot_timeseries()
-        if letter_labels:
-            pltletter = ord('a')
-            plot.labelfig(pltletter)
-            pltletter += 1
-
-        # residuals
-        ax_resid = pl.subplot(gs_rv[1, 0])
-        self.ax_list += [ax_resid]
-
-        pl.sca(ax_resid)
-        self.plot_residuals()
-        if letter_labels:
-            plot.labelfig(pltletter)
-            pltletter += 1
-
-        # phase-folded plots
-        if not nophase:
-            gs_phase = gridspec.GridSpec(self.phase_nrows, self.phase_ncols)
-
-            if self.phase_ncols == 1:
-                gs_phase.update(left=0.12, right=0.93,
-                                top=divide - self.rv_phase_space * 0.5,
-                                bottom=0.07, hspace=0.003)
-            else:
-                gs_phase.update(left=0.12, right=0.93,
-                                top=divide - self.rv_phase_space * 0.5,
-                                bottom=0.07, hspace=0.25, wspace=0.25)
-
-            for i in range(self.num_planets):
-                i_row = int(i / self.phase_ncols)
-                i_col = int(i - i_row * self.phase_ncols)
-                ax_phase = pl.subplot(gs_phase[i_row, i_col])
-                self.ax_list += [ax_phase]
-
-                pl.sca(ax_phase)
-                self.plot_phasefold(pltletter, i+1)
-                pltletter += 1
-
-        if self.saveplot is not None:
-            pl.savefig(self.saveplot, dpi=150)
-            print("RV multi-panel plot saved to %s" % self.saveplot)
-
-        return fig, self.ax_list
 
     def plot_summary(self, letter_labels=True):
         """Provision and plot a search summary plot
@@ -534,12 +297,13 @@ class PeriodModelPlot(object):
 
         # provision figure
         fig = pl.figure(figsize=(self.figwidth, figheight))
+        right_edge = 0.90
 
-        fig.subplots_adjust(left=0.12, right=0.95)
+        fig.subplots_adjust(left=0.12, right=right_edge)
         gs_rv = gridspec.GridSpec(2, 1, height_ratios=[1., 0.5])
 
         divide = 1 - self.ax_rv_height / figheight
-        gs_rv.update(left=0.12, right=0.93, top=0.93,
+        gs_rv.update(left=0.12, right=right_edge, top=0.93,
                      bottom=divide+self.rv_phase_space*0.5, hspace=0.)
 
         # orbit plot
@@ -567,19 +331,19 @@ class PeriodModelPlot(object):
         gs_phase = gridspec.GridSpec(self.phase_nrows+1, self.summary_ncols)
 
         if self.summary_ncols == 1:
-            gs_phase.update(left=0.12, right=0.93,
+            gs_phase.update(left=0.12, right=right_edge,
                             top=divide - self.rv_phase_space * 0.2,
                             bottom=0.07, hspace=0.003)
         else:
-            gs_phase.update(left=0.12, right=0.93,
+            gs_phase.update(left=0.12, right=right_edge,
                             top=divide - self.rv_phase_space * 0.2,
                             bottom=0.07, hspace=0.003, wspace=0.05)
 
         for i in range(self.num_planets):
-            #Plot phase.
-            #i_row = int(i / self.summary_ncols)
+            # Plot phase.
+            # i_row = int(i / self.summary_ncols)
             i_row = i
-            #i_col = int(i - i_row * self.summary_ncols)
+            # i_col = int(i - i_row * self.summary_ncols)
             i_col = 0
             ax_phase = pl.subplot(gs_phase[i_row, i_col])
             self.ax_list += [ax_phase]
@@ -588,7 +352,7 @@ class PeriodModelPlot(object):
             self.plot_phasefold(pltletter, i+1)
             pltletter += 1
 
-            #Plot periodogram.
+            # Plot periodogram.
             i_row = i
             i_col = 1
             ax_per = pl.subplot(gs_phase[i_row, i_col])
@@ -621,6 +385,7 @@ class PeriodModelPlot(object):
             print("Search summary plot saved to %s" % self.saveplot)
 
         return fig, self.ax_list
+
 
 class CompletenessPlots(object):
     """Class to plot results of injection/recovery tests
